2c3af7f09eaa2b3c739a5abd33016eee54f021d5
Felix Blume <137452028+fullstackopen23@users.noreply.github.com>
2023-10-19 17:01:51 +0200

    init

--- app.js
+++ app.js
@@ -29,3 +29,40 @@ projects.forEach((project) => {
     window.open(clickedProject.dataset.src)
   })
 })
+
+function handleSubmit(e) {
+  e.preventDefault()
+  return false
+}
+
+const form = document.getElementById('form')
+form.addEventListener('submit', (e) => {
+  e.preventDefault()
+  const name = document.querySelector('#nameInput')
+  const email = document.querySelector('#emailInput')
+  const message = document.querySelector('#messageInput')
+
+  const formData = new URLSearchParams({
+    name: name.value,
+    email: email.value,
+    message: message.value,
+  })
+  console.log(formData)
+  fetch('https://formspree.io/f/xleyaqor', {
+    method: 'POST',
+    body: formData,
+    headers: {
+      Accept: 'application/json',
+    },
+  }).then((res) => {
+    console.log(res)
+  })
+
+  name.value = ''
+  email.value = ''
+  message.value = ''
+  document.getElementById('success').classList.remove('hide')
+  setTimeout(() => {
+    document.getElementById('success').classList.add('hide')
+  }, 10000)
+})
